8f4cbb57008d5ea70451c687dc6734558656affa
aiXpertLab <aiXpertLab@gmail.com>
2024-08-01 20:38:20 -0400

    prod4.delivering

--- utils/delivering_classification.py
+++ utils/delivering_classification.py
@@ -1,74 +1,13 @@
 import numpy 
-import random
 import matplotlib.pyplot as plt
 
-import streamlit as st
+import streamlit as st, os
 from streamlit_extras.stateful_button import button
 
 from sklearn import preprocessing
 from sklearn.svm import SVC
 
 from utils.dataset_processing import load_dataset_with_metadata
-
-
-def show_svc(model, testX, testX_faces, out_encoder):
-    # Pick another random image
-    selection = random.choice([i for i in range(testX.shape[0])])
-    random_face_pixels = testX_faces[selection]
-    random_face_emb = testX[selection]
-    # random_face_class = testy[selection]
-    # random_face_name = out_encoder.inverse_transform([random_face_class])
-
-    # Prediction for the face
-    samples = numpy.expand_dims(random_face_emb, axis=0)
-    yhat_class = model.predict(samples)
-    yhat_prob = model.predict_proba(samples)
-
-    # Get prediction details
-    class_index = yhat_class[0]
-    class_probability = yhat_prob[0, class_index] * 100
-    predict_names = out_encoder.inverse_transform(yhat_class)
-    
-    # Display prediction
-    st.write(f'Predicted based on model: {predict_names[0]} ({class_probability:.3f})')
-    # st.write(f'Expected based on label: {random_face_name[0]}')
-
-    # Update displayed image
-    fig, ax = plt.subplots(figsize=(1, 1))
-    ax.imshow(random_face_pixels)
-    title = '%s (%.3f)' % (predict_names[0], class_probability)
-    # ax.set_title(title, fontsize=6) 
-    ax.set_xticks([])  # Remove x-axis ticks
-    ax.set_yticks([])  # Remove y-axis ticks
-    st.pyplot(fig)
-
-
-def svc(DATASIZE_NAME, EMBEDDINGS_NAME):
-        
-        data = numpy.load(DATASIZE_NAME)
-        testX_faces = data['arr_2']
-        
-        # Load face embeddings
-        data = numpy.load(EMBEDDINGS_NAME)
-        trainX, trainy, testX, testy = data['arr_0'], data['arr_1'], data['arr_2'], data['arr_3']
-
-        # Normalize input vectors
-        in_encoder = preprocessing.Normalizer(norm='l2')
-        trainX = in_encoder.transform(trainX)
-        testX = in_encoder.transform(testX)
-
-        # Label encode targets
-        out_encoder = preprocessing.LabelEncoder()
-        out_encoder.fit(trainy)
-        trainy = out_encoder.transform(trainy)
-        # testy = out_encoder.transform(testy)
-
-        # Fit model
-        model = SVC(kernel='linear', probability=True)
-        model.fit(trainX, trainy)
-
-        show_svc(model, testX, testX_faces, out_encoder)
-
 
 def svc_fitting(embeddings_training):
 
@@ -99,38 +38,6 @@ def svc_fitting(embeddings_training):
         return SVC_model, out_encoder
 
 
-def svc_matching(embeddings_training, dataset_prod, embeddings_prod):   #fitted_model, testX, testX_faces, out_encoder)
-    # Pick another random image
-    fitted_model, out_encoder = svc_fitting(embeddings_training)
-    
-    selection = random.choice([i for i in range(testX.shape[0])])   # 100th picture
-    random_face_pixels = testX_faces[selection]
-    random_face_emb = testX[selection]
-
-    # Prediction for the face
-    samples = numpy.expand_dims(random_face_emb, axis=0)
-    yhat_class = fitted_model.predict(samples)
-    yhat_prob  = fitted_model.predict_proba(samples)
-
-    # Get prediction details
-    class_index = yhat_class[0]
-    class_probability = yhat_prob[0, class_index] * 100
-    predict_names = out_encoder.inverse_transform(yhat_class)
-    
-    # Display prediction
-    st.write(f'Predicted based on model: {predict_names[0]} ({class_probability:.3f})')
-    # st.write(f'Expected based on label: {random_face_name[0]}')
-
-    # Update displayed image
-    fig, ax = plt.subplots(figsize=(1, 1))
-    ax.imshow(random_face_pixels)
-    title = '%s (%.3f)' % (predict_names[0], class_probability)
-    # ax.set_title(title, fontsize=6) 
-    ax.set_xticks([])  # Remove x-axis ticks
-    ax.set_yticks([])  # Remove y-axis ticks
-    st.pyplot(fig)
-
-
 # def classify_all_images(model, encoder, testX, testX_faces):
 def classify_all_images(embeddings_training, dataset_prod, embeddings_prod):
 
@@ -140,10 +47,8 @@ def classify_all_images(embeddings_training, dataset_prod, embeddings_prod):
     testX_faces = dataset_prod['arr_0']
 
     # Load face embeddings
-    data_train = numpy.load(embeddings_training)
     data_prod  = numpy.load(embeddings_prod)
-    trainX,trainy = data_train['arr_0'], data_train['arr_1']
-    testX,  testy = data_prod['arr_0'], data_prod['arr_1']
+    testX = data_prod['arr_0']
 
     predictions = []
     
@@ -165,4 +70,3 @@ def classify_all_images(embeddings_training, dataset_prod, embeddings_prod):
         predictions.append((predict_name, class_probability, face_pixels))
     
     return predictions
-
